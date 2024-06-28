46b3a1187d80fb8dc5a9a88e5a00fc3d6ad5f142
Gloman21 <irankundab21ertin@gmail.com>
2024-06-28 22:47:52 +0200

    other  tasks

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -1,1 +1,22 @@
 #!/usr/bin/node
+export default class Building {
+    constructor(sqft) {
+      this.sqft = sqft;
+  
+      // Prevent direct instantiation of the abstract class
+      if (new.target === Building) {
+        throw new TypeError('Cannot instantiate an abstract class');
+      }
+  
+      // Check if the subclass implements the evacuationWarningMessage method
+      this._checkEvacuationWarningMessage();
+    }
+  
+    // Separate method to check for the abstract method
+    _checkEvacuationWarningMessage() {
+      if (typeof this.evacuationWarningMessage !== 'function') {
+        throw new Error(
+          'Class extending Building must override evacuationWarningMessage',
+        );
+      } }
+  }
