f3b92d6219997bf5d2c879f2b260477f2281e8fe
Bertin Irankunda <140081862+Gloman21@users.noreply.github.com>
2024-08-22 11:05:25 +0200

    Update 6-payment_token.test.js

--- 0x06-unittests_in_js/6-payment_token.test.js
+++ 0x06-unittests_in_js/6-payment_token.test.js
@@ -1,1 +1,36 @@
 #!/usr/bin/node
+const {
+  describe, it, beforeEach, afterEach,
+} = require('mocha');
+const { expect } = require('chai');
+const getPaymentTokenFromAPI = require('./6-payment_token');
+
+describe('getPaymentTokenFromAPI', () => {
+  let response;
+  beforeEach(() => {
+    response = {
+      data: 'Successful response from the API',
+    };
+  });
+  afterEach(() => {
+    response = null;
+  });
+  it('checking with True', (done) => {
+    getPaymentTokenFromAPI(true).then((data) => {
+      expect(data).to.have.property('data');
+      expect(data.data).to.equal(response.data);
+      done();
+    })
+      .catch(done);
+  });
+  it.skip('checking with False', (done) => {
+    getPaymentTokenFromAPI(false).then(() => {
+      done();
+    })
+      .catch((err) => {
+        expect(err).to.be.an('error');
+        expect(err.message).to.equal('API call failed');
+        done();
+      });
+  });
+});
